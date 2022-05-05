af56515520faf97422e1ecd34948db6989576e9f
Ticapril <gb.oliveirati@gmail.com>
2022-05-05 16:41:13 -0300

    Finalização do projeto, precisa melhorar em alguns pontos, principalmente o css3 na versão mobile

--- app.js
+++ app.js
@@ -1,23 +1,31 @@
-// //variaveis globais
-// // a ideia do programa é listar os deputados de um determinado partido
-// var button = document.querySelector("#app button")
-// var quantidadeDeputadosExibidos = 100;
-// var siglaPartidosVetor
-// var vetorTeste
+//variaveis globais
+let idPartido 
+let membrosPartido = []
+let contador = 0
 
 //funções de criação de elementos
 function criarLista(){
-    var element = document.querySelector("div#app");
+    var element = document.querySelector("div.ml");
     lista = document.createElement("select");
     lista.setAttribute("id","listagem-partidos")
+    lista.setAttribute("class","select")
     element.appendChild(lista);
 }
 function criarItensLista(conteudo){
-    var element = document.querySelector("div.mg ul#lista-deputados");
+    var element = document.querySelector("div.ml ul#lista-deputados");
     item = document.createElement("li");
     element.appendChild(item);
     conteudoItem = document.createTextNode(conteudo)
     item.appendChild(conteudoItem)
+}
+function criarListaDeputados(seletor, tagElement){
+    let element = document.querySelector(seletor)
+    lista = document.createElement(tagElement)
+    lista.setAttribute("id","lista-deputados")
+    element.appendChild(lista)
+}
+function removerItensLista(id){
+    document.getElementById(id).remove()
 }
 function criarBotao(){
     var element = document.querySelector("div#app");
@@ -26,7 +34,6 @@ function criarBotao(){
     botaoConteudo = document.createTextNode('Listar Deputados do Partido');
     botao.appendChild(botaoConteudo)
 }
-
 function getIdPartidos(nomePartido){
     axios.get("https://dadosabertos.camara.leg.br/api/v2/partidos?ordem=ASC&ordenarPor=sigla")
     .then(function(response){
@@ -34,7 +41,8 @@ function getIdPartidos(nomePartido){
             if(nomePartido === partido.nome){
                 console.log('funcionou!')
                 console.log(partido.id)
-                return partido.id
+                //busco todos os nomes desse partido e dou um console.log
+                getMembrosPartido(partido.id)
             }
         });
     })
@@ -42,22 +50,21 @@ function getIdPartidos(nomePartido){
         console.log(e)
     })
 }
-// function getMembrosPartido(id){
-// //url https://dadosabertos.camara.leg.br/api/v2/partidos/id_parametroFunção/membros
-// axios.get(`https://dadosabertos.camara.leg.br/api/v2/partidos/${id}/membros`)
-//     .then(function(response){
-//         // console.log(response.data.dados)
-//         // criarItensLista()
-//         response.data.dados.forEach(membroPartido => {
-//             console.log(membroPartido.nome)
-//             criarItensListagemDeputados("div.mg ul#lista-deputados","li",membroPartido.nome)
-//         });
-        
-//     })
-//     .catch(function(e){
-//         console.log(e)
-//     })
-// }
+function getMembrosPartido(id){
+ //url https://dadosabertos.camara.leg.br/api/v2/partidos/id_parametroFunção/membros
+ axios.get(`https://dadosabertos.camara.leg.br/api/v2/partidos/${id}/membros`)
+     .then(function(response){
+         response.data.dados.forEach(membroPartido => {
+             membrosPartido.push(membroPartido.nome)
+             console.log(membroPartido.nome)
+             criarItensLista(membroPartido.nome)
+
+         });
+     })
+     .catch(function(e){
+         console.log(e)
+     })
+}
 function getPartidos(){
     axios.get("https://dadosabertos.camara.leg.br/api/v2/partidos?ordem=ASC&ordenarPor=sigla")
     .then(function(response){
@@ -100,19 +107,25 @@ function criarItensListagemDeputados(seletor,elementoTag,membroPartido){
     textoItem = document.createTextNode(membroPartido.nome)
     item.appendChild(textoOpcoes)
 }
+//pego a referencia do botão a ser clicado
 btn = document.getElementById("btn-listagem-deputados")
-
+//disparo uma ação que é listar os deputados do partido
 btn.addEventListener("click",function(){
-    let referencia = document.querySelector("div#app select#listagem-partidos"),conteudoReferencia
-    conteudoReferencia = referencia.value;
+    //pegar o valor do select
+    let referencia = document.querySelector("div#app select#listagem-partidos")
+    let conteudoReferencia
+    conteudoReferencia = referencia.value
     console.log(conteudoReferencia)
+    //pego esse valor e utilizo para pegar o id
+    if(contador === 0){
+        getIdPartidos(conteudoReferencia)
+        contador++
+    } else {
+        removerItensLista("lista-deputados")
+        criarListaDeputados("div.ml","ul")
+        getIdPartidos(conteudoReferencia)
+    }
     
+
 })
-
-
-getIdPartidos("Avante");
 getPartidos()
-// getDeputados()
-// getSiglaPartidos()
-
-//vou armazenar o id dos partidos em um array
